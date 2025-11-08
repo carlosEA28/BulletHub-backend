53365f7d1975dd41437d261ab23077d1a78ff9e7
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-08 09:15:58 -0300

    chore: add social config to better auth

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -3,18 +3,22 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@my-better-t-app/db";
 
 export const auth = betterAuth<BetterAuthOptions>({
-	database: prismaAdapter(prisma, {
-		provider: "postgresql",
-	}),
-	trustedOrigins: [process.env.CORS_ORIGIN || ""],
-	emailAndPassword: {
-		enabled: true,
-	},
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: "none",
-			secure: true,
-			httpOnly: true,
-		},
-	},
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ""],
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
+
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
 });
