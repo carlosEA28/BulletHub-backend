9d0ba567ed81d8fea391e157f6e758473aacb08d
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-11 14:07:43 -0300

    fix: change uploadCvSchema

--- apps/server/src/schemas/cv.ts
+++ apps/server/src/schemas/cv.ts
@@ -2,8 +2,17 @@ import z from 'zod'
 
 export const UploadCvSchema = z.object({
     title: z.string().min(1,{error:"The title is required"}),
-    cv_url: z.url().min(1,{error:"The url is required"}),
+    // cv_url: z.string().min(1,{error:"The url is required"}),
     userId: z.uuid({error:"The id is invalid"}),
 })
 
 export type UploadCvData = z.infer<typeof UploadCvSchema>;
+
+export const UploadCvRequest = z.object({
+    title: z.string().min(1,{error:"The title is required"}),
+    cv: z.file().min(1,{error:"The file is required"}),
+    userId: z.uuid({error:"The id is invalid"}),
+
+})
+
+export type UploadCvRequest = z.infer<typeof UploadCvSchema>;
