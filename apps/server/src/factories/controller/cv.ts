76d919b01f26663075bb5dc064efec59fbef9210
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-11 14:10:43 -0300

    feat: add makeUploadMainCvController

---
+++ apps/server/src/factories/controller/cv.ts
@@ -0,0 +1,18 @@
+import {CloudflareService} from "@/service/cloudflare/cloudflare";
+import {PostgresGetFileByName} from "@/repositories/postgres/cv/getFileByName";
+import {PostgresUploadCv} from "@/repositories/postgres/cv/upload_cv";
+import {UploadUserMainCvService} from "@/service/cv/upload-user-main-cv";
+import {UploadMainCVController} from "@/controller/cv/upload_main_cv";
+import {PostgresGetUserByIdRepository} from "@/repositories/postgres/user/getUserById";
+
+export const makeUploadMainCvController = () =>{
+    const cloudflare = new CloudflareService()
+    // const getUserByEmail = new PostgresGetUserByEmailRepositry()
+    const getUserById = new PostgresGetUserByIdRepository()
+    const getCvByTitle = new PostgresGetFileByName()
+    const uploadCv = new PostgresUploadCv()
+
+    const uploadCvService = new UploadUserMainCvService(cloudflare,getUserById,getCvByTitle,uploadCv)
+
+    return new UploadMainCVController(uploadCvService)
+}
