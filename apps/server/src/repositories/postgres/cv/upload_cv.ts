5ce289fe5bfdf2716c8e236a60a001cdbdb28168
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-11 14:09:45 -0300

    fix: fix UploadCvRepository cv_url field

--- apps/server/src/repositories/postgres/cv/upload_cv.ts
+++ apps/server/src/repositories/postgres/cv/upload_cv.ts
@@ -2,11 +2,11 @@ import prisma from "@my-better-t-app/db";
 import type {UploadCvData} from "@/schemas/cv";
 
 export class PostgresUploadCv{
-    async execute(uploadCvParams: UploadCvData){
+    async execute(uploadCvParams: UploadCvData,cv_url:string){
         const cv = await prisma.cv.create({
             data:{
                 title: uploadCvParams.title,
-                cv_url: uploadCvParams.cv_url,
+                cv_url: cv_url,
                 user_id:uploadCvParams.userId
             }
         })
