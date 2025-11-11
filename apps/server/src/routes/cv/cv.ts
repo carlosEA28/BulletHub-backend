4c8e191469842b0e2c19ff74e549ca0495c61b5e
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-11 17:23:41 -0300

    fix: fix cvRouter type error

--- apps/server/src/routes/cv/cv.ts
+++ apps/server/src/routes/cv/cv.ts
@@ -1,18 +1,11 @@
-import type {Request, Response} from "express";
-import {Router} from "express";
-
+import {type Request, type Response, Router} from "express";
 import {upload} from "@/middlewares/multer";
 import {makeUploadMainCvController} from "@/factories/controller/cv";
 
-// @ts-ignore
-export const cvRouter = Router()
-
+export const cvRouter = Router() as ReturnType<typeof Router>
 cvRouter.post("/",upload.single("cv"), async (req:Request,res:Response)=>{
     const cvController = makeUploadMainCvController()
 
-
-
-    // @ts-ignore
     const up = await cvController.execute(req)
 
     res.send(up)
