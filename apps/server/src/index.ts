336585d07b5e6b3cfdbf5b3233600fdcc6566028
carlosEA28 <carloseaduarte28@gmail.com>
2025-11-11 14:09:06 -0300

    feat: add dotenv config

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,11 @@
-import "dotenv/config";
+import dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
-import { auth } from "@my-better-t-app/auth";
-import { toNodeHandler } from "better-auth/node";
+import {auth} from "@my-better-t-app/auth";
+import {toNodeHandler} from "better-auth/node";
+import {cvRouter} from "@/routes/cv/cv";
+
+dotenv.config();
 
 const app = express();
 
@@ -10,7 +13,8 @@ app.use(
   cors({
     origin: [process.env.CORS_ORIGIN || "", "http://localhost:5173"],
     methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization","Cookie"],
+      exposedHeaders: ["Set-Cookie"],
     credentials: true,
   })
 );
@@ -19,7 +23,10 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
+app.use("/api/cv",cvRouter)
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+
 });
